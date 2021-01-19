f4f1b614583cc2a6327026c41fd7b4d35d5a6df8
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-19 09:18:05 -0500

    Updating requirements, tests, and functions

--- democritus_mpeg/mpeg.py
+++ democritus_mpeg/mpeg.py
@@ -1,18 +1,14 @@
-import os
-import sys
 from typing import Iterable
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-import decorators
 
 # TODO: may want to use the request_first_arg_url decorator for the mp4_download function and pass in a flag to return the response object rather than the url content
 
 
 def mp4_download(url: str) -> Iterable[bytes]:
     """."""
-    from networking import get
+    from democritus_networking import get
 
-    result = get(url, return_response_object=True)
+    result = get(url)
 
     for chunk in result.iter_content(chunk_size=255):
         if chunk:
@@ -20,4 +16,4 @@ def mp4_download(url: str) -> Iterable[bytes]:
 
 
 def mp3_download(url: str) -> Iterable[bytes]:
-    return mp4_download(url)
+    yield from mp4_download(url)
--- tests/test_mpeg.py
+++ tests/test_mpeg.py
@@ -2,7 +2,6 @@
 
 import pytest
 
-from lists import listify
 from democritus_mpeg import (
     mp4_download,
     mp3_download,
@@ -12,7 +11,7 @@ from democritus_mpeg import (
 @pytest.mark.network
 def test_mp4_download_1():
     url = 'https://archive.org/download/user-mp4-test/ac3.mp4'
-    result = listify(mp4_download(url))
+    result = tuple(mp4_download(url))
     result_bytes = b''.join(result)
     assert len(result_bytes) == 528002
 
@@ -20,6 +19,6 @@ def test_mp4_download_1():
 @pytest.mark.network
 def test_mp3_download_1():
     url = 'https://archive.org/download/testmp3testfile/mpthreetest.mp3'
-    result = listify(mp3_download(url))
+    result = tuple(mp3_download(url))
     result_bytes = b''.join(result)
     assert len(result_bytes) == 198658
