344342474a154d63719805e2a2936feac1354a4a
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-17 08:01:43 -0500

    Adding support for bumpversion
        Fixes https://github.com/democritus-project/roadmap/issues/69

--- d8s_mpeg/__init__.py
+++ d8s_mpeg/__init__.py
@@ -1,20 +1,5 @@
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
-    from importlib_metadata import version, PackageNotFoundError  # type: ignore
-
 from .mpeg import *
 
-try:
-    __version__ = version('d8s_mpeg')
-except PackageNotFoundError:
-    message = (
-        'Unable to find a version number for "d8s_mpeg". '
-        + 'This likely means the library was not installed properly. '
-        + 'Please re-install it and, if the problem persists, '
-        + 'raise an issue here: https://github.com/democritus-project/democritus-mpeg/issues.'
-    )
-    print(message)
-
+__version__ = '0.2.0'
 __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
